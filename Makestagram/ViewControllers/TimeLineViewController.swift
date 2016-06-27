e7087eec1113f8807d401de3974f3c6b012a5228
danieljku <DKu@Daniels-MacBook-Air.local>
2016-06-27 14:00:12 -0700

    First Commit

---
+++ Makestagram/ViewControllers/TimeLineViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TimeLineViewController.swift
+//  Makestagram
+//
+//  Created by Daniel Ku on 6/27/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TimelineViewController: UIViewController {
+    var photoTakingHelper: PhotoTakingHelper?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
+            if let image = image {
+                let imageData = UIImageJPEGRepresentation(image, 0.8)!
+                let imageFile = PFFile(name: "image.jpg", data: imageData)!
+                
+                let post = PFObject(className: "Post")
+                post["imageFile"] = imageFile
+                post.saveInBackground()
+            }
+        })
+    }
+}
+
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+}
